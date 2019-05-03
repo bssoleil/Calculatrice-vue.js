c59652032a6888fd730c40064e47a23ed420b51d
bssoleil <sadia.bentahar59@gmail.com>
2019-05-03 20:24:22 +0200

    added h1 and div tag

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,16 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <Calculatrice/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Calculatrice from './components/Calculatrice.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+    Calculatrice,
   }
 }
 </script>
---
+++ src/components/Calculatrice.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="calculatrice">
+    <h1>Calculatrice</h1>
+    <div class="display">0</div>
+    <div>C</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>/</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>*</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div>0</div>
+    <div>.</div>
+    <div>=</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Calculatrice",
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
